2694e2bdfaf61b5f6b15d3d3d59c29cb77b7a003
LokiiiGo <lucascalle880@gmail.com>
2026-08-08 00:44:53 -0300

    Adicionando e atualizando o texto no arquivo Sobre

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -8,7 +8,7 @@ function Sobre() {
         Lucas Bruno Calle Chambi
       </h1>
       <p className="text-lg sm:text-xl md:text-2xl text-white font-medium">
-        Freelancer Front-End
+        Desenvolvedor Freelancer Front-End
       </p>
     </section>
   )
